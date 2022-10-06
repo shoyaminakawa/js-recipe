8d5a874e09f202c147c35ae8048383a670cdd3e2
shoyaminakawa <s0620059@iwate-u.ac.jp>
2022-10-06 18:04:57 +0900

    [□と○]を学習しました。

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -11,6 +11,61 @@ const circleButton = document.getElementById("circle-button")
 //   figure.classList.remove("rounded")
 // }
 
+// figure.onclick = function () {
+//   figure.classList.toggle("rounded")
+// }
+//要素.classList.toggle(クラス名)で、クラスをadd,removeができる！
+
+/*ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー*/
+//発展★ - いろんなイベント
+//内容:クリック以外のイベントで図形を変化させる(どんなイベントでも良い)
+/*ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー*/
+
+//マウスカーソルが乗った時に丸くなり、離したときに四角に戻るプログラム
+// figure.onmouseover = function () {
+//   figure.classList.toggle("rounded")
+// }
+// figure.onmouseout = function () {
+//   figure.classList.toggle("rounded")
+// }
+
+/*ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー*/
+//発展★ - いろんなイベント
+//内容:クリック以外のイベントで図形を変化させる(どんなイベントでも良い)
+/*ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー*/
+
 figure.onclick = function () {
-  figure.classList.toggle("rounded")
+  //classにtriangleがある場合(三角の場合)
+  if (figure.classList[1] === "triangle") {
+    figure.classList.add("square")
+    //この時点ではsquareはclassList[2]にある
+  }
+  //classにsquareがある場合
+  if (figure.classList[1] === "square") {
+    //classにroudedがある場合(丸の場合)
+    if (figure.classList[2] === "rounded") {
+      figure.classList.remove("square")
+      figure.classList.add("triangle")
+      figure.classList.remove("rounded")
+    } else {
+      figure.classList.add("rounded")
+    }
+  }
+
+  if (figure.classList[2] === "square") {
+    figure.classList.remove("triangle")
+  }
 }
+
+//まずは、場合分けとして"squareがある場合を考えた。
+//そして、roundedが既にある場合は次は三角形にしたいので、squareとroudedを消去して、triangleを追加した。
+//roundedがない場合は、次は丸にしたいので、roundedを追加した。
+
+//三角形にしてから四角形にするのに苦労した。
+//まず、triangleがある場合にとりあえずsquareを追加しておく。
+//そうすると、figure.classList[1] === "square"は満たさないので、飛ばして、
+//最後のif文でtriangleを消去することで、なんとかループを作り出すことができた。
+
+//おそらく、もっと明確でわかりやすい条件分岐があるはず。。。
+
+//そのほかは、三角から四角に戻る時のアニメーションがいまいち。これはおそらくwidthとheightの関係？なのか(四角から丸は変えてないので)

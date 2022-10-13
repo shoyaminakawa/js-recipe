ee3e2a21b1be10b4c08246b9b6a3a1711b661924
shoyaminakawa <s0620059@iwate-u.ac.jp>
2022-10-13 17:12:07 +0900

    [Trello(保存機能なし)]を学習しました。

--- 02_レシピ/02_Trello（保存機能なし）/main.js
+++ 02_レシピ/02_Trello（保存機能なし）/main.js
@@ -2,6 +2,14 @@ const inputElement = document.getElementById("input-todo")
 const container = document.getElementById("cards-container")
 const addButton = document.getElementById("add-button")
 
+const inputElement2 = document.getElementById("input-todo2")
+const container2 = document.getElementById("cards-container2")
+const addButton2 = document.getElementById("add-button2")
+
+const inputElement3 = document.getElementById("input-todo3")
+const container3 = document.getElementById("cards-container3")
+const addButton3 = document.getElementById("add-button3")
+
 //追加ボタンを押した時の処理を登録
 addButton.onclick = function () {
   //カードを作成する
@@ -10,6 +18,42 @@ addButton.onclick = function () {
 
   //入力欄を空にする
   inputElement.value = ""
+}
+
+//追加ボタン2
+addButton2.onclick = function () {
+  //カードを作成する
+  const card = createCard(inputElement2.value)
+  container2.append(card)
+
+  //入力欄を空にする
+  inputElement2.value = ""
+}
+
+//追加ボタン3
+addButton3.onclick = function () {
+  //カードを作成する
+  const card = createCard(inputElement3.value)
+  container3.append(card)
+
+  //入力欄を空にする
+  inputElement3.value = ""
+}
+
+/***** 発展★ - エンターキー******/
+//入力欄でエンターキーを押した時の処理を登録
+inputElement.onkeydown = function (e) {
+  //確認できたらkeyCodeにキーコードを代入
+  if (e) {
+    if (e.keyCode === 13) {
+      //カードを作成する
+      const card = createCard(inputElement.value)
+      container.append(card)
+
+      //入力欄を空にする
+      inputElement.value = ""
+    }
+  }
 }
 //共通の処理：テキストからカードを作成する
 const createCard = function (text) {

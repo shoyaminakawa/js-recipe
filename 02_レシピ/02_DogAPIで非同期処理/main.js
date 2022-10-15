79b05708c4860790e93cbc7260253f7962a4b337
shoyaminakawa <s0620059@iwate-u.ac.jp>
2022-10-15 16:43:09 +0900

    [DogAPIで非同期処理]を学習しました。

--- 02_レシピ/02_DogAPIで非同期処理/main.js
+++ 02_レシピ/02_DogAPIで非同期処理/main.js
@@ -1,10 +1,39 @@
 const imageElement = document.getElementById("dog-image")
 
+//*****発展★ - The Cat API *****//
+/*
 // 指定したサーバーにデータを取りに行く
-fetch("https://dog.ceo/api/breeds/image/random")
+fetch(" https://api.thecatapi.com/v1/images/search")
   .then((res) => {
     return res.json() // 結果を json として読み込んで、次の then に渡す
   })
   .then((data) => {
+    console.log(data[0])
+    imageElement.src = data[0].url // 画像を表示する
+  })
+*/
+
+//***** 発展★★ - Breeds List(未完成) *****//
+let typeOfDogs = "Akita"
+
+const dogsSelect = document.getElementById("dogs-select")
+dogsSelect.onchange = function () {
+  console.log(dogsSelect.value)
+  typeOfDogs = dogsSelect.value
+}
+
+fetch(`https://dog.ceo/api/breed/${typeOfDogs}/image/random`)
+  .then((res) => {
+    console.log(res)
+    return res.json() // 結果を json として読み込んで、次の then に渡す
+  })
+  .then((data) => {
+    console.log(data)
     imageElement.src = data.message // 画像を表示する
   })
+//*****発展★ - 更新ボタン *****//
+const reloadButton = document.getElementById("reload-button")
+
+reloadButton.onclick = function () {
+  window.location.reload()
+}

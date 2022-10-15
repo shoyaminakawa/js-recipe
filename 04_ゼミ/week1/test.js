8320dd4c7a2d0fe923739a7074d18971d84d9f10
shoyaminakawa <s0620059@iwate-u.ac.jp>
2022-10-15 10:51:19 +0900

    [DogAPIで非同期処理]を学習しました。

---
+++ 04_ゼミ/week1/test.js
@@ -0,0 +1,19 @@
+const character = {
+  name: "子猫",
+  mother: {
+    name: "母猫",
+  },
+  hobbies: ["寝る", "食べる", "飼い主と遊ぶ"],
+}
+
+const multiply = function (a, b) {
+  return a * b
+}
+
+for (let i = 2; i <= 10; i += 2) {
+  console.log(i)
+}
+
+if (2) {
+  console.log("こんにちは")
+}
